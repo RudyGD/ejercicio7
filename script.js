dab2b841b413d7ccccbb10a91083c0f189233c7c
Rudy GD <gonzalezdiesterudy@gmail.com>
2021-10-14 14:40:52 +0200

    js css html

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function inicio (){
     document.getElementById("btn").addEventListener("click",  clic, false);
 }
-function clic (){
-    
-        alert("Las alertas molestan mucho");
-     
+function clic (){    
+        alert("La idea seria lanzar las capturas de cada ejercicio con su texto en una nueva ventana");     
 }
-
 window.addEventListener("load", inicio, false);
